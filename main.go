53d8058e41cf83048f543c6f0c21c7cf6f0c31d4
Eduardo Silva <eduardosilva@disroot.org>
2025-06-29 14:35:08 +0100

    Added logic to create the XDG cfg dir

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"librarian/cmd"
+	"librarian/utils"
 )
 
-type Config struct {
-}
+var (
+	CfgDir = "/.config/golibrarian/"
+)
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Failed to get user home dir: %v\n", err)
+		os.Exit(1)
+	}
+	CfgDir = filepath.Join(homeDir, CfgDir)
+
+	// Check that CfgDir exists
+	if exists, _ := utils.PathExists(CfgDir); !exists {
+		if err := os.Mkdir(CfgDir, os.ModePerm); err != nil {
+			fmt.Printf("Failed to create config dir: %s\n%v\n", CfgDir, err)
+			os.Exit(1)
+		}
+	}
+
 	cmd.Execute()
 }
